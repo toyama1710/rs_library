e81f3928b40faa27c2901fd6ee384107cdd860ce
toyama1710 <walker2002226@gmail.com>
2020-09-22 12:13:41 +0900

    add test

--- src/tests/segtree_test.rs
+++ src/tests/segtree_test.rs
@@ -14,6 +14,18 @@ impl Monoid for Min {
     }
 }
 
+struct Line();
+impl Monoid for Line {
+    type T = (i64, i64);
+    fn identity() -> Self::T {
+        return (1, 0);
+    }
+    fn op(a: &Self::T, b: &Self::T) -> Self::T {
+        return (a.0*b.0,
+                a.0*b.1 + a.1);
+    }
+}
+
 #[test]
 fn test_min() {
     let v = vec![1, 2, 4, 5, 7, 1, 8];
@@ -33,4 +45,24 @@ fn test_min() {
     assert_eq!(seg.fold(0..n), -1);
     assert_eq!(seg.fold(1..3), 2);
     assert_eq!(seg.fold(4..n), 1);
+}
+
+#[test]
+fn test_line() {
+    let mut seg = SegmentTree::<Line>::new(8);
+    let n = seg.len();
+
+    seg.update(0, (3, 1));
+
+    assert_eq!(seg.fold(0..n), (3, 1));
+
+    seg.update(0, Line::identity());
+    seg.update(2, (3, 1));
+    seg.update(4, (2, 3));
+
+    assert_eq!(seg.fold(0..n), (6, 10));
+
+    seg.update(2, (2, 3));
+    seg.update(4, (3, 1));
+    assert_eq!(seg.fold(0..n), (6, 5));
 }
